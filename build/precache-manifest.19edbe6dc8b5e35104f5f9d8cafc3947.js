01b8c6f145f838988f08e26e80e5d408906fc8c6
usman23499 <64378519+usman23499@users.noreply.github.com>
2020-11-15 02:04:13 +0500

    Add files via upload

---
+++ build/precache-manifest.19edbe6dc8b5e35104f5f9d8cafc3947.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c511b1e1f797c7d2849091f1b33ec7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb8030bdf857c86fa52a",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b33e40426d52d16ca96e",
+    "url": "/static/css/main.f36d8f96.chunk.css"
+  },
+  {
+    "revision": "bb8030bdf857c86fa52a",
+    "url": "/static/js/2.5ce4088b.chunk.js"
+  },
+  {
+    "revision": "b33e40426d52d16ca96e",
+    "url": "/static/js/main.495f991e.chunk.js"
+  },
+  {
+    "revision": "797bf6d307f8f8bc4709",
+    "url": "/static/js/runtime~main.4710b961.js"
+  },
+  {
+    "revision": "1b60c2c9af2d33eec3a0e4f71219df2f",
+    "url": "/static/media/AfterHeaderimage.1b60c2c9.jpg"
+  },
+  {
+    "revision": "5b2e62177ae1f154e0547053503d5176",
+    "url": "/static/media/Demopic.5b2e6217.jpg"
+  },
+  {
+    "revision": "9a0300207061151bc35b7093edbdba6a",
+    "url": "/static/media/image.9a030020.webp"
+  }
+]);
